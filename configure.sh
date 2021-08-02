44335317621b266b8b3d66aa005f786caf7760e4
Rodney DuPlessis <35479107+rodneydup@users.noreply.github.com>
2021-08-02 14:23:04 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,11 +4,18 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 cd "$Dir"
 
+# update submodules (juce)
 git submodule update --init --recursive
 
+# make the build directory
 mkdir build
+
+# make directories for binaries, because we told cmake to put binaries here for ease of access
 mkdir -p bin/Release
 mkdir -p bin/Debug
 
+# go into the build directory
 cd build
+
+# run the cmake configuration
 cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ..
